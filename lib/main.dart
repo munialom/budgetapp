aa9d198edf2406303fdf99205420e59b6b621614
Moses Munialo <munialom@gmail.com>
2021-12-05 19:57:06 +0300

    new budget app

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Budget App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Budget App'),
+      ),
+      body: Column(
+        children: [Text('MY HOME PAGE')],
+      ),
+    );
+  }
+}
